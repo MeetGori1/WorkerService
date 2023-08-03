f118e53e80b5d05a6ee35a9f7d1314f3984eb3e6
meet <meet.devstree@gmail.com>
2023-08-03 14:05:30 +0530

    - added button for start and stop service

--- app/src/main/java/com/example/WorkerService/MainActivity.kt
+++ app/src/main/java/com/example/WorkerService/MainActivity.kt
@@ -9,31 +9,56 @@ import androidx.work.Constraints
 import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequest
 import androidx.work.WorkManager
+import com.example.WorkerService.databinding.ActivityMainBinding
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         Handler(Looper.myLooper()!!).postDelayed({
             startWorker()
-        },1000)
+        }, 1000)
 
-        Handler(Looper.myLooper()!!).postDelayed({
+        /* Handler(Looper.myLooper()!!).postDelayed({
+             startRunningService()
+         },5000)
+ */
+
+        binding.btnStartService.setOnClickListener {
             startRunningService()
-        },5000)
+        }
+
+        binding.btnStopService.setOnClickListener {
+            stopRunningService()
+        }
     }
 
     private fun startWorker() {
-        val constraints = Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
-           val workRequest = PeriodicWorkRequest.Builder(BackgroundWorker::class.java, 15, TimeUnit.MINUTES, 1, TimeUnit.MINUTES).setConstraints(constraints).build()
+        val constraints =
+            Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+        val workRequest = PeriodicWorkRequest.Builder(
+            BackgroundWorker::class.java,
+            15,
+            TimeUnit.MINUTES,
+            1,
+            TimeUnit.MINUTES
+        ).setConstraints(constraints).build()
         WorkManager.getInstance(this).enqueue(workRequest)
     }
 
-    fun startRunningService(){
-        val intent=Intent(this,RunningService::class.java)
-        intent.action=RunningService.Actions.START.toString()
+    fun startRunningService() {
+        val intent = Intent(this, RunningService::class.java)
+        intent.action = RunningService.Actions.START.toString()
+        startService(intent)
+    }
+
+    fun stopRunningService() {
+        val intent = Intent(this, RunningService::class.java)
+        intent.action = RunningService.Actions.STOP.toString()
         startService(intent)
     }
 }
